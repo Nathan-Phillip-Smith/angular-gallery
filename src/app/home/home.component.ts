1613122de968c10467f294374ed6532913a5c8d0
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-10-03 10:22:12 -0600

    got categories working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { SharedService } from '../shared.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +16,12 @@ export class HomeComponent implements OnInit {
     'wallpapers' +
     '/photos/?per_page=40&client_id=YofUCMZ1_mklUXWkA2rdZI2PeIzXlI05p7hC4SaQwTQ';
 
-  constructor(private http: HttpClient, private sharedService: SharedService) {}
+  constructor(
+    private http: HttpClient,
+    private sharedService: SharedService,
+    private cdr: ChangeDetectorRef,
+    private dataService: DataService
+  ) {}
 
   ngOnInit(): void {
     this.getImages();
@@ -29,5 +35,20 @@ export class HomeComponent implements OnInit {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  onCategoryClicked(categoryName: string) {
+    let items = this.dataService.getCategories();
+    console.log(items);
+    console.log(categoryName);
+    let item = items.filter((e) => e.name === categoryName);
+    console.log(item[0]);
+    this.url =
+      'https://api.unsplash.com/topics/' +
+      item[0].slug +
+      '/photos/?per_page=40&client_id=YofUCMZ1_mklUXWkA2rdZI2PeIzXlI05p7hC4SaQwTQ';
+    this.getImages().then(() => {
+      this.cdr.detectChanges();
+    });
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DataService } from '../data.service';
 interface Category {
   name: string;
@@ -16,6 +16,8 @@ export class NavbarComponent implements OnInit {
   bold = 'wallpapers';
   url = ''; // initialize with your default URL
 
+  @Output() categoryClicked: EventEmitter<string> = new EventEmitter();
+
   constructor(private dataService: DataService) {} // Inject DataService here
 
   ngOnInit(): void {
@@ -28,5 +30,10 @@ export class NavbarComponent implements OnInit {
       category.slug +
       '/photos/?per_page=30&client_id=YofUCMZ1_mklUXWkA2rdZI2PeIzXlI05p7hC4SaQwTQ';
     this.bold = category.slug;
+    this.categoryClicked.emit(category.name);
+  }
+
+  onCategoryClicked(categoryName: string): void {
+    this.categoryClicked.emit(categoryName);
   }
 }
